7c30b0cb14668c5ac8c9d99498fe992070d2da2d
bhargavdas-io <bhargavdas77@hotmail.com>
2025-07-03 07:44:17 +0000

    Game logic complete and functioning as expected

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ function computerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
   const randomchoice = Math.floor(Math.random() * choices.length); // Randomly generates a number (index) from 0 and the range of the array of items,and rounds off to the nearest number
   const selection = choices[randomchoice]; // Selects the array item at the particular index number generated above.
+  console.log(selection);
   return selection;
 }
 
@@ -14,12 +15,26 @@ function userChoice() {
 
 const userreturn = userChoice();
 
+// Game Logic
+
 let result = "";
 
 if (returnpc == userreturn) {
-  result = "Tie";
+  result = "Tie :|";
+} else if (userreturn == "Rock" && returnpc == "Scissor") {
+  result = "You Win :)";
+} else if (userreturn == "Rock" && returnpc == "Paper") {
+  result = "You Lose :)";
+} else if (userreturn == "Paper" && returnpc == "Scissor") {
+  result = "You Lose :(";
+} else if (userreturn == "Paper" && returnpc == "Rock") {
+  result = "You Win :)";
+} else if (userreturn == "Scissor" && returnpc == "Rock") {
+  result = "You Lose :(";
+} else if (userreturn == "Scissor" && returnpc == "Paper") {
+  result = "You Win :)";
 } else {
-  result = "Testline";
+  result = "Please enter a valid choice";
 }
 
 console.log(result);
