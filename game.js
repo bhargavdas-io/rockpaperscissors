0db0af1bf6bd7951012713555aad8e785c06a390
bhargavdas-io <bhargavdas77@hotmail.com>
2025-07-03 06:52:08 +0000

    Initialise logic for choice comparision

--- game.js
+++ game.js
@@ -1,5 +1,3 @@
-let user_input = alert("Please enter either: 'Rock', 'Paper' or 'Scissors'"); // Asks user for input
-
 function computerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
   const randomchoice = Math.floor(Math.random() * choices.length); // Randomly generates a number (index) from 0 and the range of the array of items,and rounds off to the nearest number
@@ -7,4 +5,21 @@ function computerChoice() {
   return selection;
 }
 
-console.log(computerChoice());
+const returnpc = computerChoice();
+
+function userChoice() {
+  const userchoice = prompt("Choose between 'Rock', 'Paper' and 'Scissor'");
+  return userchoice;
+}
+
+const userreturn = userChoice();
+
+let result = "";
+
+if (returnpc == userreturn) {
+  result = "Tie";
+} else {
+  result = "Testline";
+}
+
+console.log(result);
