1edb39de13fb841890a473407b292726cbef86b1
bhargavdas-io <bhargavdas77@hotmail.com>
2025-07-03 11:40:28 +0000

    Completed and added  a 5 round match with final score count

--- game.js
+++ game.js
@@ -1,56 +1,61 @@
 function userChoice() {
-  const input = prompt("Choose between 'Rock', 'Paper' and 'Scissor'");
-  let userchoice = input.toUpperCase();
-  return userchoice;
+  let input = prompt("Choose between 'Rock', 'Paper' and 'Scissor'");
+  return input.toUpperCase();
 }
-
-const userreturn = userChoice();
 
 function computerChoice() {
   const choices = ["Rock", "Paper", "Scissor"];
   const randomchoice = Math.floor(Math.random() * choices.length); // Randomly generates a number (index) from 0 and the range of the array of items,and rounds off to the nearest number
   const selection = choices[randomchoice]; // Selects the array item at the particular index number generated above.
   console.log(selection);
-  return selection;
+  return selection.toUpperCase();
 }
-
-const returnpc = computerChoice();
-
-// Game Logic
-
-let result = "";
-
-if (returnpc == userreturn) {
-  result = "Tie :|";
-} else if (userreturn == "ROCK" && returnpc == "Scissor") {
-  result = "You Win :)";
-} else if (userreturn == "ROCK" && returnpc == "Paper") {
-  result = "You Lose :)";
-} else if (userreturn == "PAPER" && returnpc == "Scissor") {
-  result = "You Lose :(";
-} else if (userreturn == "PAPER" && returnpc == "Rock") {
-  result = "You Win :)";
-} else if (userreturn == "SCISSOR" && returnpc == "Rock") {
-  result = "You Lose :(";
-} else if (userreturn == "SCISSOR" && returnpc == "Paper") {
-  result = "You Win :)";
-} else {
-  result = "Please enter a valid choice";
-}
-
-console.log(result);
-
-// Score keeping logic for both human and pc.
 
 let userScore = 0;
 let pcScore = 0;
 
-if (result == "You Win :)") {
-  userScore++;
-} else if (result == "You Lose :(") {
-  pcScore++;
+// Game Logic
+
+for (i = 1; i <= 5; i++) {
+  const userreturn = userChoice();
+  const returnpc = computerChoice();
+  let result = "";
+
+  if (returnpc === userreturn) {
+    result = "Tie";
+  } else if (userreturn === "ROCK" && returnpc === "SCISSOR") {
+    result = "You Win";
+    userScore++;
+  } else if (userreturn == "ROCK" && returnpc == "PAPER") {
+    result = "You Lose";
+    pcScore++;
+  } else if (userreturn == "PAPER" && returnpc == "SCISSOR") {
+    result = "You Lose";
+    pcScore++;
+  } else if (userreturn == "PAPER" && returnpc == "ROCK") {
+    result = "You Win";
+    userScore++;
+  } else if (userreturn == "SCISSOR" && returnpc == "ROCK") {
+    result = "You Lose";
+    pcScore++;
+  } else if (userreturn == "SCISSOR" && returnpc == "PAPER") {
+    result = "You Win";
+    userScore++;
+  } else {
+    result = "Not a valid choice";
+  }
+
+  console.log(`Round ${i}: ${result}`);
+  console.log(`Current Score => You: ${userScore} | PC: ${pcScore}`);
 }
 
-let score = `Your Score: ${userScore} and PC Score: ${pcScore}`;
+console.log("Final Scores:");
+console.log(`You Scored: ${userScore} and PC Scored: ${pcScore}`);
 
-console.log(score);
+if (userScore > pcScore) {
+  console.log("Congrats! you win the match");
+} else if (userScore < pcScore) {
+  console.log("You Lost the match");
+} else {
+  console.log("You and PC tied the match");
+}
